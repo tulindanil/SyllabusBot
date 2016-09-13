ef9c5f1d7273eb252a43d4af4d62776961d3b85e
tulindanil <tulindanil@gmail.com>
2016-09-13 15:29:40 +0300

    Generate feedback function for every weekday

--- main.py
+++ main.py
@@ -23,16 +23,13 @@ from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
-def monday(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="monday")
+def answer_weekday(bot, update, weekday):
+    bot.sendMessage(chat_id=update.message.chat_id, text=str(s.day(weekday)))
 
-monday_handler = CommandHandler('monday', monday)
-dispatcher.add_handler(monday_handler)
-
-def tuesday(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="tuesday")
-
-tuesday_handler = CommandHandler('tuesday', tuesday)
-dispatcher.add_handler(tuesday_handler)
+weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
+for weekday in weekdays:
+    feedback = lambda bot, update, weekday=weekday: answer_weekday(bot, update, weekday)
+    handler = CommandHandler(weekday, feedback)
+    dispatcher.add_handler(handler)
 
 updater.start_polling()
