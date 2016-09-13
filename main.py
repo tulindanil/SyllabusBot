01dec262499b9557fa9baf40c4d0cab4ef62549b
tulindanil <tulindanil@gmail.com>
2016-09-13 20:34:10 +0300

    Make better structuring

--- main.py
+++ main.py
@@ -26,17 +26,22 @@ class Worker:
         weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
         for weekday in weekdays:
             def feedback(bot, update, weekday=weekday):
-                self.answer_weekday(bot, update, weekday)
+                self.answer_weekday(weekday, bot, update)
             handler = CommandHandler(weekday, feedback)
             dispatcher.add_handler(handler)
 
     def work(self):
         self.updater.start_polling()
 
-    def new_user(self, bot, update):
-        bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    def new_user(self, *args):
+        text = "I'm a bot, please talk to me!"
+        self.send_message(text, *args)
 
-    def answer_weekday(self, bot, update, weekday):
-        bot.sendMessage(chat_id=update.message.chat_id, text=str(self.s.day(weekday)))
+    def answer_weekday(self, weekday, *args):
+        self.send_message(weekday, *args)
 
+    def send_message(self, text, *args):
+        bot, update = args
+        bot.sendMessage(chat_id=update.message.chat_id,
+                        text=text)
 Worker().work()
